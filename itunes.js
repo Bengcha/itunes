8143c8f2a7a445af5bb5398d7297b6272364dae6
Beng Cha <bengcha04@gmail.com>
2017-02-02 12:31:00 -0600

    newchange

--- itunes.js
+++ itunes.js
@@ -1,26 +1,28 @@
-function ItunesService(){
+$(document).ready(function() 
+{
+    $("#startSearch").click(function() 
+    {
+        searchMusic();
+    });
 
-    this.getMusicByArtist = function(artist, cb) {
-      
-      var url = '//bcw-getter.herokuapp.com/?url=';
-      var url2 = 'https://itunes.apple.com/search?term=' + artist;
-      var apiUrl = url + encodeURIComponent(url2);
-      
-      $('#get-music-button').text('LOADING....');
-      
-      return $.getJSON(apiUrl).then(function(response){
-        var songList = response.results.map(function (song) {
-                  return {
-                      title: song.trackName,
-                      albumArt: song.artworkUrl60,
-                      artist: song.artistName,
-                      collection: song.collectionName,
-                      price: song.trackPrice,
-                      preview: song.previewUrl
-                    };
-                })
-        $('#get-music-button').text('GET MUSIC');
-        return songList;
-      })
+    var searchMusic = function() {
+        var userInput = $("#searchBox").val();
+        $.ajax({
+            type: "GET",
+            dataType: "jsonp",
+            url: "https://itunes.apple.com/search?term=" + userInput,
+            success: function(data) {
+            $("#searchBox").val("");
+            displayMusicInformation();
+
+            }
+        });
     }
+});
+
+function displayMusicInformation()
+{
+    var information = val();
+    information = $(".musicImage")
+
 }
